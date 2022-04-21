26ae3ffe24767864f3f913c2844de8990d5d54b6
heavnz0r <heavnzor@protonmail.com>
2022-04-21 16:00:41 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -7,9 +7,10 @@ $(function () {
        - Chaque joueur possède un score temporaire (ROUND) et un score global (GLOBAL). 
        - À chaque tour, le joueur a son ROUND initialisé à 1 et peut lancer un dé autant de fois qu'il le souhaite. 
        - Le résultat d’un lancer est ajouté au ROUND.
-       - Lors de son tour, le joueur peut décider à tout moment de:-Cliquer sur l’option “Hold”, qui permet d’envoyer les points du ROUND vers le GLOBAL. 
-       - Ce sera alors le tour de l’autre joueur. -Lancer le dé. S’il obtient un 1, son score ROUND est perdu et c’est la fin de son tour. 
-       - Le premier joueur qui atteint les 100 points sur global gagne le jeu.
+       - Lors de son tour, le joueur peut décider à tout moment de:
+          ° Cliquer sur l’option “Hold”, qui permet d’envoyer les points du ROUND vers le GLOBAL. Ce sera alors le tour de l’autre joueur. 
+          ° Lancer le dé. S’il obtient un 1, son score ROUND est perdu et c’est la fin de son tour. 
+       - Le premier joueur qui atteint les 100 points sur son GLOBAL gagne le jeu.
   */
 
 
